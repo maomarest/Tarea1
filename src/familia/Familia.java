863756bc80ee05ce9734b110ffd8317517df1f5b
Mao <Mao@Mauro>
2016-03-09 18:30:48 -0500

    Emmanuel Vanegas Arias, Mauricio Martinez Estrada y Sebastian Gil Version 1.0

--- src/familia/Familia.java
+++ src/familia/Familia.java
@@ -1,6 +1,14 @@
+/**
+ * @author Emmanuel Vanegas Arias, Mauricio Martinez Estrada y Sebastian Gil
+ * @since 09/03/2016
+ * @version 1.0
+ */
+
 package familia;
 import java.util.*;
-
+/**
+     * Se identifica a cada familia con el apellido del padre. 
+     */
 public class Familia {
 
     private String apellido;
